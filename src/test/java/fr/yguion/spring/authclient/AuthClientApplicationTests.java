ebf47409e30791ad815ec066be2e7b0b9a423c3c
Yoann Guion <yguion@enovacom.fr>
2020-03-29 00:19:00 +0100

    sample oauth client application

---
+++ src/test/java/fr/yguion/spring/authclient/AuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.yguion.spring.authclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
