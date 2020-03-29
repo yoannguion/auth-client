93d40b4fc3a1faf14a696eec6d85632df2c7f208
Yoann Guion <yguion@enovacom.fr>
2020-03-29 18:31:13 +0200

    authorization code flow with pkce

--- src/main/java/fr/yguion/spring/authclient/SecurityConfiguration.java
+++ src/main/java/fr/yguion/spring/authclient/SecurityConfiguration.java
@@ -3,15 +3,33 @@ package fr.yguion.spring.authclient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+import static org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfiguration(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**").authorizeRequests()
             .antMatchers("/", "/login**").permitAll()
             .anyRequest().authenticated()
             .and()
-            .oauth2Login();
+            .oauth2Login()
+            .authorizationEndpoint()
+            .authorizationRequestResolver(new CustomAuthorizationRequestResolver(
+            clientRegistrationRepository, DEFAULT_AUTHORIZATION_REQUEST_BASE_URI
+        ));
+
+
+
     }
 }
